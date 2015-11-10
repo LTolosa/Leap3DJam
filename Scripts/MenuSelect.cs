42c51384a091db318d1355d306d9c4c2271084d0
Ernesto Sandoval <escrnesto@gmail.com>
2015-11-09 23:17:36 -0500

    Derp.

---
+++ Scripts/MenuSelect.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using LMWidgets;
+
+public class MenuSelect : MonoBehaviour {
+
+    public ButtonDemo mode;
+    public bool hard = false;
+
+    // Use this for initialization
+    void Start () {
+        mode.StartHandler += OnSelect;
+    }
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnSelect(object sender, LMWidgets.EventArg<bool> arg) {
+      if (arg.CurrentValue)
+      {
+          if (hard)
+          {
+              PlayerPrefs.SetInt("Difficulty", 1);
+              Application.LoadLevel(1);
+          }
+          else
+          {
+              PlayerPrefs.SetInt("Difficulty", 0);
+							Application.LoadLevel(1);
+          }
+      }
+  }
+}
